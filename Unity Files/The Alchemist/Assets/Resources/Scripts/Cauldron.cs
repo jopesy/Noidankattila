d3890a843fca6969feee3d8538aca09e14947bf0
Joonas Syysirta <joonsyys@gmail.com>
2015-10-08 01:08:46 +0300

    Add ingredient pickup
    - Added ingredient pickup (adds picked up ingredients to inventory).
    - Added tooltip to dragged ingredients in cauldron room.
    - Cauldron room shelf slots will now refill automatically, based on
    ingredients the player has in his inventory.

---
+++ Unity Files/The Alchemist/Assets/Resources/Scripts/Cauldron.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Cauldron : MonoBehaviour {
+
+	public List<Item> potionIngredients;
+	private Item potion;
+	private ItemDatabase database;
+	private Inventory inventory;
+
+	// Use this for initialization
+	void Start(){
+		potionIngredients = new List<Item> ();
+	}
+
+	void Update(){
+		// If there are 3 ingredients in the cauldron
+		if (potionIngredients.Count == 3) {
+			MakePotion ();
+			print("Potion created!");
+		}
+	}
+	// Make a potion out of the ingredients in the cauldron,
+	// add it into player's inventory and empty the cauldron
+	void MakePotion(){
+		database = GameObject.FindGameObjectWithTag("Item Database").GetComponent<ItemDatabase>();
+		inventory = GameObject.FindGameObjectWithTag("Inventory").GetComponent<Inventory>();
+		potion = database.items[3];
+		inventory.AddItem(potion.itemID);
+		this.potionIngredients.Clear();
+	}
+}
---
+++ Unity Files/The Alchemist/Assets/Resources/Scripts/IngredientPickup.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IngredientPickup : MonoBehaviour {
+
+	public int itemID;
+	private Inventory inventory;
+
+	void Start(){
+		inventory = GameObject.FindGameObjectWithTag("Inventory").GetComponent<Inventory>();
+	}
+	void OnTriggerEnter(Collider other){
+		if (other.tag == "Player") {
+			inventory.AddItem(this.itemID);
+			print("Item picked up!");
+			Destroy(gameObject);
+		}
+	}
+}
