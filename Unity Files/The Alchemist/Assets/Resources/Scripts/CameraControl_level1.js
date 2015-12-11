fa2da64c79cdd378aaf4b22fc1d1dfff47b1116a
sisyfos123 <marko.halonen@gmail.com>
2015-12-11 02:21:58 +0200

    Level 1 should be pretty good now
    I fixed some issues etc. I did not test all the way to the castle part
    << Bad jumper.

--- Unity Files/The Alchemist/Assets/Resources/Scripts/CameraControl_level1.js
+++ Unity Files/The Alchemist/Assets/Resources/Scripts/CameraControl_level1.js
@@ -7,7 +7,7 @@ var lift = 1.5;
 
 function Update () {
     transform.position = target.position + Vector3(0, lift, distance);
-    transform.position.y = Mathf.Clamp(target.transform.position.y + lift, -4, 100.0);
+    transform.position.y = Mathf.Clamp(target.transform.position.y + lift, -0, 100.0);
     //Mathf.Clamp(target.position.y, 0.0, 0.0);
     //target.position = pos;
 	//transform.LookAt(target);
